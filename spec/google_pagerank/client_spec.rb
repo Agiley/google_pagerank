eefa7ed53400a9a2eaabdbfcdf90bf904f6a37b4
Sebastian Johnsson <sebastian@agiley.se>
2015-03-05 23:25:07 +0100

    Syntax update

--- spec/google_pagerank/client_spec.rb
+++ spec/google_pagerank/client_spec.rb
@@ -3,11 +3,12 @@ require File.expand_path('../../spec_helper', __FILE__)
 describe Google::Pagerank::Client do
   describe "when initialized" do
     before(:each) do
-      @client = Google::Pagerank::Client.new
+      @client   =   Google::Pagerank::Client.new
     end
         
     it "should correctly fetch pagerank for google.com" do
-      expect(@client.pagerank('google.com')).to eq(9)
+      pagerank  =   @client.pagerank('google.com')
+      expect(pagerank).to eq(9)
     end
   end
 end
